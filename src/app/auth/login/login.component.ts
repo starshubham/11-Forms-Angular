4901bfe51011e905db49338178268bdb21e4d1d5
SHUBHAM SETH <shubham.seth@greenpaynetwork.co.in>
2026-07-13 23:53:26 +0530

    Interacting With The Underlying Form Object In The Component

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { afterNextRender, Component, DestroyRef, inject, viewChild } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -9,6 +10,27 @@ import { FormsModule, NgForm } from '@angular/forms';
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  private form = viewChild.required<NgForm>('form');
+  private destroyRef = inject(DestroyRef);
+  
+  constructor() {
+    afterNextRender(() => {
+      const subscription = this.form()
+        .valueChanges?.pipe(debounceTime(500))
+        .subscribe({
+          next: (value) => 
+            window.localStorage.setItem(
+              'saved-login-form',
+              JSON.stringify({email: value.email})
+            )
+        });
+
+      this.destroyRef.onDestroy(() => {
+        subscription?.unsubscribe();
+      });
+    });
+  }
+
   onSubmit(formData: NgForm) {
     if (formData.form.invalid) {
       return;
@@ -19,5 +41,7 @@ export class LoginComponent {
 
     console.log(formData.form);
     console.log(enteredEmail, enteredPassword);
+
+    formData.form.reset();
   }
 }
